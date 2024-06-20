1fa5f26a5f023c78ad04ba22150dd2d0d45b7445
Alex <alex@Dominics-MacBook-Air.local>
2024-06-20 14:12:15 -0500

    arrays-excersises-1,2,3,4

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -3,8 +3,28 @@ let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
-//1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+//1) Print the result of using concat on the two arrays. Does concat alter the original arrays?Verify this by printing holdCabinet1 after using the method.
+// NO
+holdCabinet1.concat(holdCabinet1, holdCabinet2);
+console.log(holdCabinet1);
 
-//2) Print a slice of two elements from each array. Does slice alter the original arrays?
+//2) Print a slice of two elements from each array. Does slice alter the original arrays? 
+// NO
 
-//3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+console.log(holdCabinet1.slice(0,2));
+console.log(holdCabinet1);
+
+console.log(holdCabinet2.slice(1,3));
+console.log(holdCabinet2);
+
+//3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays? 
+/* YES. Both rearrange, but reverse rearranges according to the opposite of the original order, 
+while sort rearranges sequentially, starting with numbers from least to greatest, then strings in alphabetical order. */
+
+console.log(holdCabinet1);
+console.log(holdCabinet1.reverse());
+console.log(holdCabinet1);
+
+console.log(holdCabinet2);
+console.log(holdCabinet2.sort());
+console.log(holdCabinet2);
