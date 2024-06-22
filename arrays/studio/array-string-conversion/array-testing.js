79070c5e40dce936e14d01d8e3daa60401e40312
Alex <alex@Dominics-MacBook-Air.local>
2024-06-22 00:25:15 -0500

    The time I did git add with two dots

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -8,39 +8,57 @@ strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
+	let check = (protoArray1 + protoArray2 + protoArray3 + protoArray4).includes(','); 
 	let output;
+
 	//TODO: 2. write the code required for this step
 
+	if (check === true) {
+		output = strings[0].split(',').reverse().join(',');
+	}
+
+	
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
+	let check = (protoArray1 + protoArray2 + protoArray3 + protoArray4).includes(';');
 	let output;
 //TODO: write the code required for this step
+if (check === true) {
+	output = strings[1].split(';').sort().join('-');
+}
 
-  
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
+	let check = (protoArray1 + protoArray2 + protoArray3 + protoArray4).includes(' '); 
 	let output;
+
   //TODO: write the code required for this step
+
+if (check === true) {
+	output = strings[2].split(' ').sort().reverse().join(' ');
+}
 
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
+	let check = (protoArray1 + protoArray2 + protoArray3 + protoArray4).includes(', '); 
 	let output;
+	
 	//TODO: write the code required for this step
   
+	if (check === true) {
+		output = strings[3].split(', ').reverse().join(',');
+	}
+
 	return output;
 }
 
--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -5,10 +5,29 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+let cabinet1 = food.split(',').sort()
+let cabinet2 = equipment.split(',').sort()
+let cabinet3 = pets.split(',').sort()
+let cabinet4 = sleepAids.split(',').sort()
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+
+let cargoHold = [cabinet1, cabinet2, cabinet3, cabinet4];
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
 
+const input = require('readline-sync');
+
+let info = input.question("Please select cabinet (1 - 4): ");
+
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
+if (4 >= info >= 1) {
+    console.log(`Cabinet ${info} contains ${cargoHold[info -= 1]} `);
+} else {
+    console.log(`ERROR! Cabinet ${info} does not exist. Please try cabinets 1 - 4`);
+}
+
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
