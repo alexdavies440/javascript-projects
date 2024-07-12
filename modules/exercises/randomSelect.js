fb1b319a73bba3ec2eb3cd79b08ff3de04c49b14
Alex <alex@Dominics-MacBook-Air.local>
2024-07-11 23:59:31 -0500

    Modules Exercises

--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,17 @@
 function randomFromArray(arr){
+let i = Math.floor(Math.random() * arr.length);
+return arr[i];
   //Your code here to select a random element from the array passed to the function.
 }
+
+let testArray = ['a', 'b', 'c', 'd', 'e', 'f'];
+function testTheFunction() {
+  let result;
+  for (let j = 0; j < 21; j++) {
+    console.log(randomFromArray(testArray));
+  }
+
+}
+console.log(testTheFunction(testArray));
 
 //TODO: Export the randomFromArray function.
