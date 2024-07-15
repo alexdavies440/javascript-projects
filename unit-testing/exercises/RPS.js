50839832b55c87ed4e7a1b8fcf2c689e30710f42
Alex <alex@Dominics-MacBook-Air.local>
2024-07-15 01:34:36 -0500

    Unit Tests Exercises found the typo and updated code

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -12,11 +12,16 @@ function whoWon(player1,player2){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'scissors' && player2 === 'rock '){
+   if (player1 === 'scissors' && player2 === 'rock'){
      return 'Player 2 wins!';
    }
- 
-   return 'Player 1 wins!';
+   let plays = ['rock', 'paper', 'scissors'];
+   if (plays.includes(player1) === false || plays.includes(player2) === false) {
+    return 'Bruh...';
+   }
+   return 'Player 1 wins!'
  }
+
+ console.log(whoWon('pap3r', 'r0ck'));
 
  module.exports = whoWon;
--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -1,8 +1,25 @@
 const whoWon = require('../RPS');
 
 describe("whoWon", function() {
-    test("Determines who wins in Rock, Paper, Scissors", function() {
+    test("whoWon returns a tie when both arguments are equal to each other", function() {
         let output = whoWon('rock', 'rock');
         expect(output).toEqual('TIE!');
     });
+    test("whoWon returns winner for 'rock against scissors", function() {
+        let output = whoWon('scissors', 'rock');
+        expect(output).toEqual('Player 2 wins!')
+    });
+    test("whoWon returns winner for 'scissors' against 'paper'", function() {
+        let output = whoWon('paper', 'scissors');
+        expect(output).toEqual('Player 2 wins!');
+    });
+    test("whoWon returns winner for 'paper' against 'rock'", function() {
+        let output = whoWon('rock', 'paper');
+        expect(output).toEqual('Player 2 wins!');
+    })
+    test("whoWon returns error conveying disappointment if players try to go off-script", function() {
+        let output = whoWon('corndog', '42');
+        expect(output).toEqual('Bruh...');
+    })
+    
 });
