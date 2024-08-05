87bec1cdd5fe0196198cec0a4a4b11682e482567
Alex <alex@Dominics-MacBook-Air.local>
2024-08-04 21:40:08 -0500

    Dom and Events Exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,29 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+
+    // let liftoff = function() {
+    //     document.getElementById("statusReport").innerHTML = "Houston, we have liftoff!";
+    //     console.log('Houston, we have liftoff!');
+    // }
+
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+     });
+     missionAbort.addEventListener("mouseover", function() {
+         missionAbort.style.backgroundColor = "red";
+         missionAbort.style.color = "darkred"
+     });
+     missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+        missionAbort.style.color = "";
+     });
+     missionAbort.addEventListener("click", function() {
+        if (window.confirm("Are you sure you want to abort the mission?") === true) {
+            window.alert("Mission aborted! Space shuttle returning home");
+        }
+     });
+} 
+
 
 window.addEventListener("load", init);
