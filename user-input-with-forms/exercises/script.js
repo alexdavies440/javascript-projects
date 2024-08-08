715a462c327135d0d8444a6c6597b5eb9c0ea282
alexdavies440 <alexdavies440@gmail.com>
2024-08-08 00:03:00 -0500

    started bonus mission. Need sleep

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,9 @@
 //Code Your Solution Below
+let rocketSimulation = document.getElementById("rocketSimulation");
+
+window.addEventListene("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function() {
+
+    })
+})
